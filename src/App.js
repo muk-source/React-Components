2dfaf2d4ae30fa4e43defc4405508a111e45d86a
Mukund22-moona <64807797+Mukund22-moona@users.noreply.github.com>
2024-03-01 21:30:42 +0530

    form validations added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import AccordionContainer from "./components/Accordion/AccordionContainer";
 import Counter from "./components/Auto-Increment-Counter/Counter";
 import Calculator from "./components/Calculator/Calculator";
 import CarouselContainer from "./components/Carousel/CarouselContainer";
+import Form1 from "./components/Form/Form1";
 import Loader from "./components/Loader/Loader";
 import StarRating from "./components/StarRating/StarRating";
 import Fetch from "./components/useFetch/Fetch";
@@ -17,7 +18,8 @@ function App() {
       {/* <Calculator/> */}
       {/* <Loader/> */}
       {/* <ThemeContainer/> */}
-      <Fetch/>
+      {/* <Fetch/> */}
+      <Form1/>
     </div>
   );
 }
